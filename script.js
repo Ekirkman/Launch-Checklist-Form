ab2fb5937d62dbe5c57ce2f6b654a1672235b040
Ekirkman <edwardk110@gmail.com>
2020-07-26 17:52:15 -0500

    refactored

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.addEventListener("load",function (){
          console.log(json);
          let missionTarget = document.getElementById("missionTarget");
          let index = 0;
-          function randomPick(json){
+         function randomPick(json){
               index = Math.floor(Math.random()*json.length)
             };
             randomPick(json);
@@ -26,65 +26,54 @@ window.addEventListener("load",function (){
    });
    form.addEventListener("submit",function(event){
       event.preventDefault();
-      console.log("SUBMITTED");
       let pilotName= document.querySelector("input[name=pilotName]").value;
-       console.log(typeof pilotName);
       let coPilotName = document.querySelector("input[name=copilotName]").value;
-       console.log(coPilotName);
       let fuelLevel = document.querySelector("input[name='fuelLevel']").value;
-       console.log(fuelLevel);
       let cargoMass = document.querySelector("input[name=cargoMass]").value;
-      console.log(cargoMass);
-
-         let coPilotType = isNaN(Number(coPilotName));
-         let cargoType = isNaN(Number(cargoMass));
-         let pilotType = isNaN(Number(pilotName));
-         let fuelType = isNaN(Number(fuelLevel));
-
-
-         let faultyItems = document.getElementById("faultyItems");
-         let launchStatus = document.getElementById("launchStatus");
-         let fuelMessage = "high enough";
-         let cargoMessage = "low enough";
+      let coPilotType = isNaN(Number(coPilotName));
+      let cargoType = isNaN(Number(cargoMass));
+      let pilotType = isNaN(Number(pilotName));
+      let fuelType = isNaN(Number(fuelLevel));
+      let faultyItems = document.getElementById("faultyItems");
+      let launchStatus = document.getElementById("launchStatus");
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let fuelMessage = "high enough";
+      let cargoMessage = "low enough";
 
          if(fuelType || cargoType || !pilotType || !coPilotType){
             alert("Invalid Input");
          }
          else if(cargoMass < 10000 && fuelLevel > 10000){
-         launchStatus.innerHTML = "<h2>Shuttle Ready for Launch</h2>";
-         launchStatus.style.color="green";
-         faultyItems.style.visibility="visible";
-         faultyItems.innerHTML = `<ol>
-         <li id="pilotStatus">Pilot ${pilotName} is ready for launch</li>
-         <li id="copilotStatus">Co-pilot ${coPilotName} is ready for launch</li>
-         <li id="fuelStatus">Fuel level  ${fuelMessage} for launch</li>
-         <li id="cargoStatus">Cargo mass ${cargoMessage} for launch</li>
-     </ol>`
-     
+            launchStatus.innerHTML = "<h2>Shuttle Ready for Launch</h2>";
+            launchStatus.style.color="green";
+            faultyItems.style.visibility="visible";
+            pilotStatus.innerHTML =` ${pilotName} is ready for launch`;
+            copilotStatus.innerHTML =`${coPilotName} is ready for launch`;
+            fuelStatus.innerHTML = `${fuelMessage} for launch`;
+            cargoStatus.innerHTML =`${cargoMessage} for launch`;
          }
          else if(cargoMass > 10000 || fuelLevel < 10000){
             launchStatus.innerHTML = "<h2>Shuttle Not Ready for Launch</h2>";
             launchStatus.style.color="red";
             faultyItems.style.visibility="visible";
-            if(fuelLevel < 10000){
+            if (fuelLevel < 10000){
                fuelMessage = "too low"
             }
             if(cargoMass > 10000){
                cargoMessage = "too high"
             }
-            faultyItems.innerHTML = `<ol>
-            <li id="pilotStatus">${pilotName} is ready for launch</li>
-            <li id="copilotStatus">${coPilotName} is ready for launch</li>
-            <li id="fuelStatus">Fuel level ${fuelMessage} for launch</li>
-            <li id="cargoStatus">Cargo ${cargoMessage} for launch</li>
-        </ol>`
-            } 
-            
-          
+            pilotStatus.innerHTML =` ${pilotName} is ready for launch`;
+            copilotStatus.innerHTML =`${coPilotName} is ready for launch`;
+            fuelStatus.innerHTML = `${fuelMessage} for launch`;
+            cargoStatus.innerHTML =`${cargoMessage} for launch`;
+         } 
    });
             
 
-   });
+});
 
 
 
